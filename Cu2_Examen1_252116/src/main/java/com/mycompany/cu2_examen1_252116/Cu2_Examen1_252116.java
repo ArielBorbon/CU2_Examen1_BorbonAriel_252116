8d25db6be5db800d128db51e235d353704072a2b
ArielBorbon <ariel.borbon252116@potros.itson.edu.mx>
2025-10-08 05:07:45 -0700

    Crear proyecto java

---
+++ Cu2_Examen1_252116/src/main/java/com/mycompany/cu2_examen1_252116/Cu2_Examen1_252116.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cu2_examen1_252116;
+
+/**
+ *
+ * @author PC Gamer
+ */
+public class Cu2_Examen1_252116 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
